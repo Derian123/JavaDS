66a5e9afcffc7d77151cb0cfd69aa98d77e8e0d7
Derian123 <derian.escobar@yahoo.com>
2017-10-10 13:23:17 -0400

    Node Class for Linked List

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+/**
+ * Created by derianescobar on 10/10/17.
+ */
+public class Node {
+
+    int data;
+
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
