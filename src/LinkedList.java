ff67e0eedbe2efe5145a79b8f684969c8d9550c2
Derian123 <derian.escobar@yahoo.com>
2017-10-12 13:23:51 -0400

    LinkedList Class with interview problem included within

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -6,7 +6,7 @@
  */
 public class LinkedList {
 
-    Node head;
+    Node  head;
 
     public void append(int data){
 
@@ -79,10 +79,98 @@ public class LinkedList {
 
     }
 
+    //This method returns the length of the linkedList
+    public int getLength(){
+
+        //Count increments as it iterates through
+        int count = 0;
+
+        //Takes into account if the LinkedList is empty
+        if(isEmpty()){
+
+            return count;
+        }
+
+        //Current will go through the list
+        Node current = head;
+
+        //Goes until the nest node is null
+        while(current.next != null){
+
+            //Points to the next node then increments count
+            current = current.next;
+            count++;
+
+        }
+
+        return count;
+    }
+
+    //Prints out everything in the linked list
+    public String toString(){
+
+        //returns it in a string
+        String newStr = "Contains:\n";
+
+        Node current = head;
+
+        while(current.next != null){
+
+            //Goes to the next node
+            current = current.next;
+            //Then grabs the data
+            newStr += current.data + "\n";
+
+        }
+
+        return newStr;
+    }
+
 
     //Checks to see if the linkedList is empty
     public boolean isEmpty(){
 
         return head == null;
     }
+
+    //This method find the nth to last element on the linkedlist
+    public int nthToLast(int n){
+
+        //To get the index we substract the length from the input
+        int index = getLength() - n;
+
+        //Checks to see if the list is empty
+        if(isEmpty()){
+
+            return -1;
+        }
+
+        //Sets current equal to the head
+        Node current = head;
+
+        //For loop that keep going until i reaches the index
+        for(int i = 0; i <= index; i++){
+
+            //This will keep iterating until the for loop eneds
+            current = current.next;
+        }
+
+        //Returns the nth element on the link
+        return current.data;
+
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList l1 = new LinkedList();
+        l1.append(35);
+        l1.append(43);
+        l1.append(54);
+        l1.append(5);
+        l1.append(4);
+        l1.append(9);
+        l1.append(8);
+        System.out.println(l1.nthToLast(3));
+    }
+
 }
