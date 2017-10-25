b3aa581abcc448bbeceea3b651196da2e1eb92b0
Derian123 <derian.escobar@yahoo.com>
2017-10-25 13:29:17 -0400

    Added a add linkedlist function

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -51,6 +51,8 @@ public class LinkedList {
 
     }
 
+
+
     public void delete(int data){
 
         //Checks to see if the linkedlist is empty
@@ -61,13 +63,11 @@ public class LinkedList {
 
         //Special case where the head is the data to delete
         //If it is then the head goes to the next available node
-        if(head.data == data){
+        if(head.data == data) {
 
             head = head.next;
 
         }
-
-
         Node current = head;
         //Goes until current.next does not equal null
         while(current.next != null){
@@ -128,11 +128,7 @@ public class LinkedList {
             newStr += current.data + "\n";
             //Goes to the next node
             current = current.next;
-
-
-
         }
-
         return newStr;
     }
 
@@ -177,16 +173,80 @@ public class LinkedList {
 
     }
 
+    //Adds 2 linked list which represents a number in reverse
+    public static Node adding(Node head1, Node head2){
+
+        /**
+         *
+         * These variables represent pointers so that the linkedlist doesn't get deleted
+         */
+        Node current1 = head1;
+        Node current2 = head2;
+
+        Node head3 = new Node(-1);
+        Node current3 = head3;
+
+        //This count will be used to do all calculations
+        int count = 0;
+
+        //While current1 or current 2 does not equal to null
+        //This is due to take into account different sized linked list
+        while(current1 != null || current2 != null){
+
+            //Checks to see if current 1 is not null
+            if(current1!= null){
+
+                //Increment count by the data
+                count+= current1.data;
+                //Go to the next available node
+                current1 = current1.next;
+            }
+
+            //Checks to see if current 2 is not null
+            if(current2!= null){
+
+                //Increment count by the data
+                count+= current2.data;
+                //Go to the next available node
+                current2 = current2.next;
+            }
+
+            //Current3 will make a new node where thus making the list
+            //Mod 10 incase of double digits so you need to get the last number
+            current3.next = new Node(count%10);
+
+            //Gets rid of the number that was just placed within the node
+            count/=10;
+
+            //Moves to the next node made.
+            current3 = current3.next;
+        }
+
+        //if count still has a remainder of 1
+        if(count == 1){
+
+            //Creates a new node based on that count
+            current3.next = new Node(count);
+        }
+
+        //Returns head.next
+        return head3.next;
+
+
+    }
+
     public static void main(String[] args) {
 
         LinkedList l1 = new LinkedList();
         l1.appendBefore(4);
         l1.append(3);
         l1.append(43);
+        l1.append(2);
         l1.appendBefore(1);
         l1.delete(3);
         l1.delete(1);
         l1.append(32);
+        l1.delete(4);
 
         System.out.println(l1.toString());
     }
