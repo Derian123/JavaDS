aa3dbb189e6f6ea3b4a6d407fed3aaf1fc36d97b
Derian123 <derian.escobar@yahoo.com>
2017-11-07 12:56:09 -0500

    Binary search tree node

---
+++ src/BSTNode.java
@@ -0,0 +1,22 @@
+/**
+ * Created by derianescobar on 11/6/17.
+ */
+public class BSTNode {
+
+    int data;
+    BSTNode leftChild;
+    BSTNode rightChild;
+    BSTNode parent;
+
+
+    public BSTNode(){
+
+    }
+
+    public BSTNode(int data){
+
+        this.data = data;
+    }
+
+
+}
